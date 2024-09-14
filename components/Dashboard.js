1cafcdc8e9fb282a2f1000301bf29a1a899da1c1
Gracy <gracy10.2003@gmail.com>
2024-09-14 21:47:21 +0530

    updated dashboard.js

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -28,7 +28,7 @@ export default function Dashboard() {
         }
       }
     }
-    return { num_days: total_number_of_days, average_mood: sum_moods / total_number_of_days }
+    return { num_days: total_number_of_days, average_mood: (sum_moods / total_number_of_days).toFixed(2) }
   }
 
   const statuses = {
